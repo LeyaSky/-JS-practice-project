ce91538d51dea9b7b0edcc2f231d073be7459f97
Leya <kalashnikova.ekat@mail.ru>
2023-03-14 04:34:47 +0300

    task 3 - movie rating

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ let numberOfFilms = prompt('Сколько фильмов вы уже посмо
 
 const personalMovieDB = {
     count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
 }
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+function movieInput() {
+    let movie = prompt('Один из последних просмотренных фильмов?');
+    let movieRating = prompt('На сколько оцените его?');
+    personalMovieDB.movies[movie] = movieRating;
+}
+
+movieInput();
+movieInput();
+console.log(personalMovieDB.movies);
